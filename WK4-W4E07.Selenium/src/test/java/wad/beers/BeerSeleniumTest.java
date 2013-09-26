6bb2ac7c04b5d4657debadf1787be91ffdf1b2d8
Timo Sand <timo.j.sand@gmail.com>
2013-09-26 13:53:44 +0300

    Completed E7

--- WK4-W4E07.Selenium/src/test/java/wad/beers/BeerSeleniumTest.java
+++ WK4-W4E07.Selenium/src/test/java/wad/beers/BeerSeleniumTest.java
@@ -1,10 +1,12 @@
 package wad.beers;
 
+import junit.framework.Assert;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class BeerSeleniumTest {
@@ -27,4 +29,40 @@ public class BeerSeleniumTest {
     }
 
     // PROGRAM YOUR TESTS HERE!
+    
+    @Test
+    public void submitAndVerify() {
+        driver.get(baseUrl+"/beers");
+        
+        assertFalse(driver.getPageSource().contains("Up Up Down Down Left Right Left Right BA Select"));
+        WebElement element = driver.findElement(By.id("name"));
+        element.sendKeys("Up Up Down Down Left Right Left Right BA Select");
+        element.submit();
+        assertTrue(driver.getPageSource().contains("Up Up Down Down Left Right Left Right BA Select"));
+        
+    }
+    
+    @Test
+    public void submitThreeAndVerify() {
+        driver.get(baseUrl+"/beers");
+        assertFalse(driver.getPageSource().contains("Gargamel Ale"));
+        assertFalse(driver.getPageSource().contains("Crazy Ivan"));
+        assertFalse(driver.getPageSource().contains("Hoptimus Prime"));
+        
+        WebElement element = driver.findElement(By.id("name"));
+        element.sendKeys("Gargamel Ale");
+        element.submit();
+        
+        element = driver.findElement(By.id("name"));
+        element.sendKeys("Crazy Ivan");
+        element.submit();
+        
+        element = driver.findElement(By.id("name"));
+        element.sendKeys("Hoptimus Prime");
+        element.submit();
+        
+        assertTrue(driver.getPageSource().contains("Gargamel Ale"));
+        assertTrue(driver.getPageSource().contains("Crazy Ivan"));
+        assertTrue(driver.getPageSource().contains("Hoptimus Prime"));
+    }
 }

b81898024282be4dcd3685f97a98a3de557fe054
Timo Sand <timo.j.sand@gmail.com>
2013-10-06 19:02:46 +0300

    Finished W5E2

--- WK5-W5E02.CachedItemStorage/src/main/java/wad/itemstorage/ItemStorageController.java
+++ WK5-W5E02.CachedItemStorage/src/main/java/wad/itemstorage/ItemStorageController.java
@@ -2,6 +2,8 @@ package wad.itemstorage;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,24 +19,28 @@ public class ItemStorageController {
 
     @RequestMapping(value = "items", method = RequestMethod.GET)
     @ResponseBody
+    @Cacheable("items")
     public List<Item> getItems() {
         return itemRepository.findAll();
     }
 
     @RequestMapping(value = "items/{id}", method = RequestMethod.GET)
     @ResponseBody
+    @Cacheable("items")
     public Item getItem(@PathVariable Long id) {
         return itemRepository.findOne(id);
     }
 
     @RequestMapping(value = "items", method = RequestMethod.POST)
     @ResponseBody
+    @CacheEvict(value = "items", allEntries = true)
     public Item postItem(@RequestBody Item item) {
         return itemRepository.save(item);
     }
 
     @RequestMapping(value = "items/{id}", method = RequestMethod.DELETE)
     @ResponseBody
+    @CacheEvict(value = "items")
     public Item deleteItem(@PathVariable Long id) {
         Item item = itemRepository.findOne(id);
         itemRepository.delete(id);
